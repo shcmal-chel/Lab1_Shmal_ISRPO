74e781b65e2d550e6546872f08f79d107f55ed94
Ivan <shcmalivan@gmail.com>
2025-09-21 17:04:15 +0300

    Add files via upload

---
+++ Classroom_work-master/Classroom_work/Program.cs
@@ -0,0 +1,128 @@
+﻿namespace zadanie_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.
+
+            //Console.Write("Введите вашу любимую игру: ");
+            //string game = Console.ReadLine();
+
+            //Console.Write("Введите ваш любимый фильм: ");
+            //string movie = Console.ReadLine();
+
+            //Console.Write("Введите ваше любимое аниме: ");
+            //string anime = Console.ReadLine();
+            //Console.WriteLine($"Ваша любимая игра - {game}, ваш любимый фильм - {movie} и ваше любимое аниме - {anime}");
+
+            //Console.Write("Введите вашу любимую цифру: ");
+            //int number = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Ваше любимое число - {number}");
+
+            //Console.Write("Введите число пи: ");
+            //double pi = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"Число пи - {pi}");
+
+            //Console.Write("Введите вашу любимую букву: ");
+            //string bukva = Console.ReadLine();
+            //Console.WriteLine($"Ваша любимая буква - {bukva}");
+
+            ////Задание 2.
+
+            //Console.WriteLine("I need more power!");
+            //Console.WriteLine("I need more power!");
+            //Console.WriteLine("I need more power!");
+            //Console.WriteLine("I need more power!");
+
+            ////Задание 3.
+
+            //Console.WriteLine("Hello There");
+
+            //Задание 4.
+
+            //Console.WriteLine("Введите стоимость монитора первого компьютера: ");
+            //int monitor_1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость системного блока первого компьютера: ");
+            //int system_block_1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость клавиатуры первого компьютера: ");
+            //int keyboard_1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость мыши первого компьютера: ");
+            //int mouse_1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость монитора второго компьютера: ");
+            //int monitor_2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость системного блока второго компьютера: ");
+            //int system_block_2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость клавиатуры второго компьютера: ");
+            //int keyboard_2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость мыши второго компьютера: ");
+            //int mouse_2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость монитора третьего компьютера: ");
+            //int monitor_3 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость системного блока третьего компьютера: ");
+            //int system_block_3 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость клавиатуры третьего компьютера: ");
+            //int keyboard_3 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Введите стоимость мыши третьего компьютера: ");
+            //int mouse_3 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Стоимость покупки трех компьютеров: ");
+            //Console.Write((monitor_1+system_block_1+keyboard_1+mouse_1)+
+            //    (monitor_2 + system_block_2 + keyboard_2 + mouse_2)+ 
+            //   (monitor_3 + system_block_3 + keyboard_3 + mouse_3));
+
+            //Задание 5.
+
+            //Console.Write("Введите первое значение: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("Введите второе значение: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.Write("Значение функции: ");
+            //Console.WriteLine(3*(Math.Pow(a+b,3))+275*(Math.Pow(b,2))-127*a-41);
+
+            //Задание 6.
+
+            //Console.Write("Введите свое имя: ");
+            //string name = Console.ReadLine();
+
+            //Console.Write("Введите свой возраст: ");
+            //int age = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Привет {0}!",name);
+            //Console.WriteLine("Сейчас тебе {0}.",age);
+            //Console.WriteLine("В следующем году будет {0}.",age+1);
+
+            //Задание 7.
+
+            //Console.Write("Введите свое имя: ");
+            //string name = Console.ReadLine();
+
+            //Console.Write("Введите свой возраст: ");
+            //int age = int.Parse(Console.ReadLine());
+
+            //Console.Write("Введите свой город: ");
+            //string city = Console.ReadLine();
+
+            //Console.WriteLine("Привет, {0}! Тебе {1} лет и ты живешь в городе {2}.",name,age,city);
+
+            //Задание 8.
+
+            //Console.Write("Введите температуру в Цельсиях: ");
+            //double temperature = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Температура в Фаренгейтах: {0}°F", (temperature*9/5)+32);
+        }
+    }
+}
